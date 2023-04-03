60548511f01d1390b61657704b021f7963db1809
VanUliK <vanulik@work.com>
2023-04-03 16:12:54 +0200

    reload

--- price.js
+++ price.js
@@ -1,64 +1,23 @@
 fetch("./price.json")
   .then((response) => response.json())
   .then((data) => priceShow(data))
+  // .then((data) => console.log(data))
   .catch((error) => console.error(error));
 
 function priceShow(data) {
-  console.log();
-  // const templateEl = document.querySelector('.template');
-  // const itemGroup = document.querySelector('.featuredItems');
+  // Object.entries(data);
+
+  console.log(data)
+  let arr = Object.entries(data);
 
 
-  // for (let index = 0; index < 6; index++) {
+  for (let index = 0; index < 16; index++) {
+    let part = data[index];
+    let arr = Object.entries(part);
 
-  //   const element = data[index];
-  //   const itemEl = templateEl.content
-  //     .querySelector(".item_block")
-  //     .cloneNode(true);
+    // console.log(part.value);
+    // console.log(part.element);
+    // pillars.value = data[2]
+  };
 
-  //   const featuredItemAttribute = itemEl.querySelector(".featuredItem");
-  //   featuredItemAttribute.setAttribute('data-id', element.id);
-  //   featuredItemAttribute.setAttribute('data-name', element.head);
-  //   featuredItemAttribute.setAttribute('data-price', element.price);
-  //   const headingEl = itemEl.querySelector(".featuredName");
-  //   headingEl.textContent = element.head;
-  //   const dscItem = itemEl.querySelector(".featuredText");
-  //   dscItem.textContent = element.description;
-  //   const priceEl = itemEl.querySelector(".featuredPrice");
-  //   const redSize = priceEl.querySelector('.featuredPrice_color');
-  //   redSize.textContent = '$' + element.price.toFixed(2);
-  //   const imgEl = itemEl.querySelector(".featuredImg");
-  //   imgEl.src = element.image;
-  //   itemGroup.appendChild(itemEl)
-  // };
-
-
-  // clickButton.addEventListener('click', function (e) {
-  //   const templateEl = document.querySelector('.template');
-  //   const itemGroup = document.querySelector('.featuredItems');
-  //   for (let index = 6; index < 12; index++) {
-
-  //     const element = data[index];
-  //     const itemEl = templateEl.content
-  //       .querySelector(".item_block")
-  //       .cloneNode(true);
-
-  //     const featuredItemAttribute = itemEl.querySelector(".featuredItem");
-  //     featuredItemAttribute.setAttribute('data-id', element.id);
-  //     featuredItemAttribute.setAttribute('data-name', element.head);
-  //     featuredItemAttribute.setAttribute('data-price', element.price);
-  //     const headingEl = itemEl.querySelector(".featuredName");
-  //     headingEl.textContent = element.head;
-  //     const dscItem = itemEl.querySelector(".featuredText");
-  //     dscItem.textContent = element.description;
-  //     const priceEl = itemEl.querySelector(".featuredPrice");
-  //     const redSize = priceEl.querySelector('.featuredPrice_color');
-  //     redSize.textContent = '$' + element.price.toFixed(2);
-  //     const imgEl = itemEl.querySelector(".featuredImg");
-  //     imgEl.src = element.image;
-  //     itemGroup.appendChild(itemEl)
-  //   };
-
-  //   clickButton.classList = 'hidden'
-  // });
 }
